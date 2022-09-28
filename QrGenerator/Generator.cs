7d9018a0d0dede6f582f861aa1f135eef59d0aef
Luis Alfredo Figueroa Bracamontes <92luisalfredo@protonmail.com>
2022-09-27 19:26:14 -0500

    Adding functionability to Qr player Now it changes codes each 10 seconds and there's a function to be executed each time a song finished to change information of codes

--- QrGenerator/Generator.cs
+++ QrGenerator/Generator.cs
@@ -1,8 +1,13 @@
-using System;
+using Net.Codecrete.QrCodeGenerator;
+using System.Windows.Media;
 
 namespace QrGenerator
 {
     public static class Generator
     {
+        public static DrawingImage GenerateQr(string text) =>
+            QrCodeDrawing.CreateDrawing(QrCode.EncodeText(text,
+                    QrCode.Ecc.High),
+                192);
     }
 }
--- Stereux/SongInfoContainer.xaml.cs
+++ Stereux/SongInfoContainer.xaml.cs
@@ -1,4 +1,7 @@
 ﻿using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Threading;
+using Connections.Models;
 using Connections.SongsDSTableAdapters;
 
 namespace Stereux
@@ -8,9 +11,56 @@ namespace Stereux
     /// </summary>
     public partial class SongInfoContainer : Page
     {
+        private int _infoIndex;
+
+        // For each array, the order is 1. Stereux, 2. Source, 3. Song
+        private string[] _titles = { "Download Stereux", "Music provided by:", "Get more info" };
+
+        private string[] _subtitles = { "from here!", string.Empty, "about this song" };
+
+        private DrawingImage[] _qrCodes =
+        {
+            QrGenerator.Generator.GenerateQr("https://github.com/LuisAlfredo92/Stereux"),
+            QrGenerator.Generator.GenerateQr("https://github.com/LuisAlfredo92/Stereux"),
+            QrGenerator.Generator.GenerateQr("https://github.com/LuisAlfredo92/Stereux")
+        };
+
         public SongInfoContainer()
         {
+            _infoIndex = 0;
             InitializeComponent();
+            titleLabel.Text = _titles[_infoIndex];
+            subtitleLabel.Text = _subtitles[_infoIndex];
+            qrCodeImage.Source = _qrCodes[_infoIndex];
+            DispatcherTimer timer = new()
+            {
+                Interval = TimeSpan.FromSeconds(10)
+            };
+            timer.Tick += ChangeView;
+            timer.Start();
+        }
+
+        private void ChangeVars(Sources source, string songUrl)
+        {
+            _subtitles[1] = source switch
+            {
+                Sources.Ncs => "No Copyright Sounds",
+                _ => "Unknown"
+            };
+            _qrCodes[1] = source switch
+            {
+                Sources.Ncs => QrGenerator.Generator.GenerateQr("https://ncs.io"),
+                _ => QrGenerator.Generator.GenerateQr("")
+            };
+            _qrCodes[2] = QrGenerator.Generator.GenerateQr(songUrl);
+        }
+
+        private void ChangeView(object? sender, EventArgs e)
+        {
+            _infoIndex = _infoIndex < 2 ? _infoIndex + 1 : 0;
+            titleLabel.Text = _titles[_infoIndex];
+            subtitleLabel.Text = _subtitles[_infoIndex];
+            qrCodeImage.Source = _qrCodes[_infoIndex];
         }
     }
 }
